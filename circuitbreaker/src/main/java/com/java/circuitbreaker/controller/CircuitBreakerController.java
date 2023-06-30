d7fd7ecd5f0497689176dfda01a2d3be065e99d2
sgairola1981 <sgairola@gmail.com>
2023-06-30 18:35:06 +0530

    First commit

---
+++ circuitbreaker/src/main/java/com/java/circuitbreaker/controller/CircuitBreakerController.java
@@ -0,0 +1,31 @@
+package com.java.circuitbreaker.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@RestController
+public class CircuitBreakerController {
+
+    Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/getInvoice")
+    @CircuitBreaker(name = "getInvoiceCB", fallbackMethod = "getInvoiceFallback")
+    public String getInvoice() {
+        logger.info("getInvoice() call starts here");
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:8080/invoice/rest/find/2",
+                String.class);
+        logger.info("Response :" + entity.getStatusCode());
+        return entity.getBody();
+    }
+
+    public String getInvoiceFallback(Exception e) {
+        logger.info("---RESPONSE FROM FALLBACK METHOD---");
+        return "SERVICE IS DOWN, PLEASE TRY AFTER SOMETIME !!!";
+    }
+}
